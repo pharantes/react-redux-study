18b1d2a7af1d21c3e5df03ba283f1a295bca7334
Pedro Arantes <pedro.hga90@gmail.com>
2021-03-25 12:06:41 +0100

    started songs-redux

---
+++ songs-redux/src/components/SongList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const SongList = () => {
+  return <div>SongList</div>;
+};
+
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
---
+++ songs-redux/src/index.js
@@ -0,0 +1,17 @@
+// 3rd party dependencies
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+// App code
+
+import App from "./components/App";
+import reducers from "./reducers";
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
